5f989a0edfe0194b836a18e8ed58b4f2825c858e
pavmx <pavmxh@gmail.com>
2026-05-17 02:16:51 +0500

    feat: wire TronWatcher into server startup (skipped in test env)

--- casino-backend/src/index.ts
+++ casino-backend/src/index.ts
@@ -4,6 +4,7 @@ import { createApp } from './app'
 import { env } from './config/env'
 import { prisma } from './config/prisma'
 import { redis } from './config/redis'
+import { startTronWatcher } from './services/tronWatcher.service'
 
 async function main() {
   await redis.connect()
@@ -14,6 +15,10 @@ async function main() {
 
   httpServer.listen(env.PORT, () => {
     console.log(`Backend running on http://localhost:${env.PORT}`)
+    if (env.NODE_ENV !== 'test') {
+      startTronWatcher()
+      console.log('TronWatcher: polling TronGrid every 10s')
+    }
   })
 }
 
